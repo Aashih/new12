dd83aefa8aec801fa71e045eff62daa71ae675e7
Ashish <ashishpanchtilak@gmail.com>
2025-05-11 20:18:52 +0530

    vite

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "http://localhost:4000", // Your backend API
         changeOrigin: true,
         secure: false,
       },
     },
+    allowedHosts: [
+      'new12-2-ib79.onrender.com',  // Add your Render domain here
+    ],
   },
+  plugins: [react()],
 });
