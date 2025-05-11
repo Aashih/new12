ca363eb99f03856828d79082856f7256e8dd1063
Ashish <ashishpanchtilak@gmail.com>
2025-05-11 21:36:44 +0530

    dashboard

--- Dashbord1/vite.config.js
+++ Dashbord1/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
-      // Forward /api/* requests to your backend
-      '/api': {
-        target: 'http://localhost:4000',
+      "/api": {
+        target: "http://localhost:4000", // Your backend API
         changeOrigin: true,
         secure: false,
       },
     },
+    allowedHosts: [
+      'dashboard.onrender.com',  // Add your Render domain here
+    ],
   },
-})
+  plugins: [react()],
+});
